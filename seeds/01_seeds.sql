deda213da79e14b52c9214607cb4a627a97b64c3
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-11 14:47:32 -0400

    Insert temp data into ussers table

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,7 @@
+/* Insert temp data to users table */
+INSERT INTO users (name, email, password)
+VALUES ('user1', 'user1@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+       ('user2', 'user2@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+       ('user3', 'user3@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+       ('user4', 'user4@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+       ('user5', 'user5@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
