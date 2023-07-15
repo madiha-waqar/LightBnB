96882e8bb0d8a0defbea8556e269964ed854ea30
madiha-waqar <mail2madihawaqar@gmail.com>
2023-07-15 17:01:59 -0400

    Update addProperty to create new listing by adding new property

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -156,7 +156,7 @@ const getAllProperties = (options, limit = 10) => {
     // show result for condition: cost_per_night <= maximum_price_per_night
     queryString += `cost_per_night <= $${queryParams.length} `;
   }
-  
+
   // move 'GROUP BY' before 'HAVING' clause to maintain SQL order of execution
   queryString += `
   GROUP BY properties.id  `;
@@ -190,10 +190,49 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id,
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+  ];
+  // Insert a new property into the "properties" table and return the inserted row
+  return pool
+    .query(`INSERT INTO properties (owner_id,
+            title,
+            description,
+            thumbnail_photo_url,
+            cover_photo_url,
+            cost_per_night,
+            street,
+            city,
+            province,
+            post_code,
+            country,
+            parking_spaces,
+            number_of_bathrooms,
+            number_of_bedrooms)
+            
+            VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+            RETURNING *`, queryParams)
+
+    .then((result) => {
+      // The promise resolves with the inserted row of property 
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 module.exports = {
